e2cbb5b2184a2c04e8291cdce27e1a14d4b0c644
dhivishya1998 <dhivishyasanthosh@gmail.com>
2025-11-03 19:29:26 +0530

    time

--- src/org/bank/BankInfo.java
+++ src/org/bank/BankInfo.java
@@ -3,16 +3,18 @@ package org.bank;
 public class BankInfo extends AxisBank{
 	private void saving() {
 		System.out.println("Savings");
-
 	}
 	private void fixed() {
 		System.out.println("Fixed");
+	}
+	@Override
+	public void deposit() {
+		super.deposit();
 	}
 	public static void main(String[] args) {
 		BankInfo bank1 = new BankInfo();
 		bank1.saving();
 		bank1.fixed();
-		bank1.deposit();
+		bank1.deposit();		
 	}
-
 }
