2332aa15e42874cef4cf031f02e7cc1e98007bb4
dhivishya1998 <dhivishyasanthosh@gmail.com>
2025-11-03 19:11:50 +0530

    commit message

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,18 @@
+package org.bank;
+
+public class BankInfo extends AxisBank{
+	private void saving() {
+		System.out.println("Savings");
+
+	}
+	private void fixed() {
+		System.out.println("Fixed");
+	}
+	public static void main(String[] args) {
+		BankInfo bank1 = new BankInfo();
+		bank1.saving();
+		bank1.fixed();
+		bank1.deposit();
+	}
+
+}
